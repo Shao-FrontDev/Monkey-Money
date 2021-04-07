3ced0c8326b65b92bc20c1b4f90cd8e15ebc8eb5
Richard <shaodeyu@foxmail.com>
2021-04-07 21:32:59 +0800

    创建Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,56 @@
 import React from "react";
-import styled from "styled-components";
 import {
   HashRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-import Nav from "components/Nav";
-
-const Wrapper = styled.div`
-  /* border: 1px solid red; */
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Main = styled.div`
-  /* border: 1px solid green; */
-  flex-grow: 1;
-`;
+import Layout from "components/Layout";
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Redirect exact from="/" to="money"></Redirect>
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags />
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Redirect exact from="/" to="money"></Redirect>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 function Statistics() {
-  return <h2>统计页面</h2>;
+  return (
+    <Layout>
+      <h2>统计页面</h2>
+    </Layout>
+  );
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return (
+    <Layout>
+      <h2>标签页面</h2>
+    </Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账页</h2>;
+  return (
+    <Layout>
+      <h2>记账本</h2>
+    </Layout>
+  );
 }
 
 function NoMatch() {
---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+const Wrapper = styled.div`
+  /* border: 1px solid red; */
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  /* border: 1px solid green; */
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+type Props = {
+  name: string;
+};
+
+export default function Layout(props: any) {
+  return (
+    <Wrapper>
+      <Main>{props.children}</Main>
+      <Nav />
+    </Wrapper>
+  );
+}
