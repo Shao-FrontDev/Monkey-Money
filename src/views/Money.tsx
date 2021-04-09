3c0af4da20ae22c1d458be139d257fde1747387b
Richard <shaodeyu@foxmail.com>
2021-04-09 11:16:03 +0800

    完成备注的功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,7 +2,7 @@ import Layout from "components/Layout";
 import React from "react";
 import styled from "styled-components";
 import { CategorySection } from "./Money/CategorySection";
-import { NotesSection } from "./Money/NotesSection";
+import { NoteSection } from "./Money/NoteSection";
 import { NumberPadSection } from "./Money/NumberPadSection";
 import { TagsSection } from "./Money/TagsSection";
 
@@ -17,12 +17,9 @@ export default function Money() {
       <TagsSection>
    
       </TagsSection>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注" />
-        </label>
-      </NotesSection>
+      <NoteSection>
+
+      </NoteSection>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
