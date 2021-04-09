47c5fdd9c1510a853c2bd7e8389a2bcc13603045
Richard <shaodeyu@foxmail.com>
2021-04-09 19:58:36 +0800

    重构表单数据收集

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -21,34 +21,31 @@ export default function Money() {
     amount: 0,
   });
 
+  type Selected = typeof selected;
+  const onChange = (obj: Partial<Selected>) => {
+    setSelected({
+      ...selected,
+      ...obj,
+    });
+  };
+
   return (
     <MyLayout>
-      {selected.tags}
-      <br />
-      {selected.note}
       <TagsSection
         value={selected.tags}
-        onChange={(tags) => {
-          setSelected({ ...selected, tags: tags });
-        }}
+        onChange={(tags) => onChange({ tags })}
       ></TagsSection>
       <NoteSection
         value={selected.note}
-        onChange={(note) => {
-          setSelected({ ...selected, note: note });
-        }}
+        onChange={(note) => onChange({ note })}
       ></NoteSection>
       <CategorySection
         value={selected.category}
-        onChange={(category) => {
-          setSelected({ ...selected, category: category });
-        }}
+        onChange={(category) => onChange({ category })}
       ></CategorySection>
       <NumberPadSection
         value={selected.amount}
-        onChange={(amount) => {
-          setSelected({ ...selected, amount: amount });
-        }}
+        onChange={(amount) => onChange({ amount })}
         onOk={() => {}}
       ></NumberPadSection>
     </MyLayout>
