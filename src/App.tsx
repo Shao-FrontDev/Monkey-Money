0498880c8a44269a3ba05e0cd55a6daf10e2eade
Richard <shaodeyu@foxmail.com>
2021-04-11 22:30:18 +0800

    添加了路由懒加载，提高性能

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import NoMatch from "views/404";
-import Money from "views/Money";
-import Statistics from "views/Statistics";
-import Tag from "views/Tag";
-import Tags from "views/Tags";
+// import NoMatch from "views/404";
+// import Money from "views/Money";
+// import Statistics from "views/Statistics";
+// import Tag from "views/Tag";
+// import Tags from "views/Tags";
+
+const NoMatch = lazy(() => import("views/404"));
+const Money = lazy(() => import("views/Money"));
+const Statistics = lazy(() => import("views/Statistics"));
+const Tag = lazy(() => import("views/Tag"));
+const Tags = lazy(() => import("views/Tags"));
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/tags">
-        <Tags />
-      </Route>
-      <Route path="/tags/:id" component={Tag}>
-        <Tag />
-      </Route>
-      <Route path="/money">
-        <Money />
-      </Route>
-      <Route path="/statistics">
-        <Statistics />
-      </Route>
-      <Redirect exact from="/" to="money"></Redirect>
-      <Route path="*">
-        <NoMatch />
-      </Route>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/tags">
+          <Tags />
+        </Route>
+        <Route path="/tags/:id" component={Tag}>
+          <Tag />
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Redirect exact from="/" to="money"></Redirect>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Suspense>
     </Switch>
   );
 }
